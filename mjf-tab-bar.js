e38fceb6a83e8535160ae44d500dc70edb97b69d
Michael Falk <michael.falk@me.com>
2019-01-26 20:52:38 -0800

    Added Font-Size

--- mjf-tab-bar.js
+++ mjf-tab-bar.js
@@ -1,3 +1,25 @@
+/**
+ * mjfTabBar
+ *
+ * mjfTabBar - Copyright (C) 2019, Michael Falk <michael.falk@me.com>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ associated documentation files (the "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ sell copies of the Software, and to permit persons to whom the Software is furnished to
+ do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+ LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+
 (function ( $ ) {
 
     $.fn.mjfTabBar = function(options) {
@@ -23,6 +45,12 @@
 
                     $('#'+settings.tabBarId+' .mjfTab').removeClass('mjfTabActive mjfTabInactive').addClass('mjfTabInactive');
                     clickedTab.toggleClass('mjfTabInactive mjfTabActive');
+
+                    if (settings.tabFontSize) {
+                        $('.mjfTab').css(
+                            'font-size', settings.tabFontSize
+                        );
+                    }
 
                     if (settings.activeTabColor) {
                         $('.mjfTab.mjfTabActive').css({
@@ -105,6 +133,12 @@
 
                 // Attach the style element to the document head
                 document.getElementsByTagName('head')[0].appendChild(styleElement);
+
+                if (settings.tabFontSize) {
+                    $('.mjfTab').css(
+                        "font-size", settings.tabFontSize
+                    );
+                }
 
                 if (settings.activeTabColor) {
                     $('.mjfTab.mjfTabActive').css({
